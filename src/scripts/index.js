b421455462a7e82fba6d2037817829781c02f198
Alexander Gabriel <agabriel.mmt-b2014@fh-salzburg.ac.at>
2018-01-06 12:00:24 +0100

    add: basic call barchart

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,13 @@
 import * as d3 from 'd3';
-import * as barchart from './charts/barchart';
-import * as treechart from './charts/treechart';
-import * as bubblechart from './charts/bubblechart';
+import { barchartCreate, barchartUpdate } from './charts/barchart';
+// import * as treechart from './charts/treechart';
+// import * as bubblechart from './charts/bubblechart';
 
+d3.json(`${window.location.href}assets/nrw_2017.json`, (error17, json17) => {
+  const data17 = json17;
+  d3.json(`${window.location.href}assets/nrw_2013.json`, (error13, json13) => {
+    const data13 = json13;
+    barchartCreate();
+    barchartUpdate(data13, data17, 'G90000');
+  });
+});
