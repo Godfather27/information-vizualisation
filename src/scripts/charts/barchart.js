b51380130e43a3b0ed9451d25aa9d4106aa11123
Alexander Gabriel <agabriel.mmt-b2014@fh-salzburg.ac.at>
2018-01-06 11:50:06 +0100

    final: barchart with linting

--- src/scripts/charts/barchart.js
+++ src/scripts/charts/barchart.js
@@ -1,21 +1,32 @@
 import * as d3 from 'd3';
 
-let xscale, yscale, groupYAxis, groupXAxis, yAxis, xAxis, g;
-const margin = {top: 10, bottom: 20, left: 80, right: 60};
+let xscale;
+let yscale;
+let groupYAxis;
+let groupXAxis;
+let yAxis;
+let xAxis;
+let g;
+const margin = {
+  top: 10,
+  bottom: 20,
+  left: 80,
+  right: 60,
+};
 const width = 800 - margin.left - margin.right;
 const height = 600 - margin.top - margin.bottom;
 const colors = {
-  "SPÖ" : 0,
-  "ÖVP" : 172,
-  "FPÖ" : 235,
-  "GRÜNE" : 120,
-  "NEOS" : 306,
-  "KPÖ" : 55,
-  "CPÖ" : 36,
-  "M" : 272,
-  "EUAUS" : 69,
-  "SLP" : 142
-}
+  SPÖ: 0,
+  ÖVP: 172,
+  FPÖ: 235,
+  GRÜNE: 120,
+  NEOS: 306,
+  KPÖ: 55,
+  CPÖ: 36,
+  M: 272,
+  EUAUS: 69,
+  SLP: 142,
+};
 
 function barchartCreate() {
   // Creates sources <svg> element
@@ -27,87 +38,80 @@ function barchartCreate() {
     .attr('transform', `translate(${margin.left},${margin.top})`);
 
   xscale = d3.scaleBand().rangeRound([0, width]).paddingInner(0.5);
-  yscale = d3.scaleLinear().range([height,0]);
+  yscale = d3.scaleLinear().range([height, 0]);
 
   xAxis = d3.axisBottom().scale(xscale);
-  groupXAxis = g.append('g').attr('class','x axis').style("transform","translate(0,"+height+"px)");
+  groupXAxis = g.append('g').attr('class', 'x axis').style('transform', `translate(0,${height}px)`);
   yAxis = d3.axisLeft().scale(yscale);
-  groupYAxis = g.append('g').attr('class','y axis');
-}
-
-function barchartUpdate(data13, data17, gemeinde){
-
-  let inputGKZ = gemeinde // todo variable
-  let common = Object.keys(data13[0]).filter(x => Object.keys(data17[0]).includes(x));
-  data13 = data13.filter( x => x.GKZ == inputGKZ)[0]
-  data17 = data17.filter( x => x.GKZ == inputGKZ)[0]
-  let nrw13 = []
-  let nrw17 = []
-
-  for(var i = 6; i < common.length; i++){
-    nrw13.push({
-      key: common[i],
-      value: data13[common[i]]
-    })
-  }
-
-  for(var i = 6; i < common.length; i++){
-    nrw17.push({
-      key: common[i],
-      value: data17[common[i]]
-    })
-  }
-  update({nrw13, nrw17});
+  groupYAxis = g.append('g').attr('class', 'y axis');
 }
 
 function update(data) {
+  xscale.domain(data.nrw17.map(d => d.key));
+  yscale.domain([0, Math.max(
+    d3.max(data.nrw13, d => d.value),
+    d3.max(data.nrw17, d => d.value),
+  )]);
 
-    xscale.domain(data.nrw17.map( d => d.key));
-    yscale.domain([0, Math.max(d3.max(data.nrw13, (d) => d.value),d3.max(data.nrw17, (d) => d.value))]);
-
-  //render the axis
   groupXAxis.call(xAxis);
   groupYAxis.call(yAxis);
-  // Render the chart with new data
-  // DATA JOIN use the key argument for ensurign that the same DOM element is bound to the same data-item
 
-
-
-  const rect = g.selectAll('.nrw13').data(data.nrw13, (d) => d.key);
-  const rect_enter = rect.enter().append('rect')
-    .attr('x', 0) //set intelligent default values for animation
+  const rect13 = g.selectAll('.nrw13').data(data.nrw13, d => d.key);
+  const rect13Enter = rect13.enter().append('rect')
+    .attr('x', 0)
     .attr('y', 0)
-    .classed("nrw13",true)
+    .classed('nrw13', true)
     .attr('width', xscale.bandwidth())
     .attr('height', 0);
-  rect_enter.append('title');
-  rect.merge(rect_enter).transition()
-    .attr('height', (d) => height - yscale(d.value))
-    .attr('y', (d) => yscale(d.value))
+  rect13Enter.append('title');
+  rect13.merge(rect13Enter).transition()
+    .attr('height', d => height - yscale(d.value))
+    .attr('y', d => yscale(d.value))
     .attr('width', xscale.bandwidth())
-    .attr('x', (d) => xscale(d.key)+25)
-    .style('fill', (d) => 'hsl('+colors[d.key]+',100%,40%)');
-  rect.merge(rect_enter).select('title').text((d) => d.key);
-  rect.exit().remove();
+    .attr('x', d => xscale(d.key) + 25)
+    .style('fill', d => `hsl(${colors[d.key]},100%,40%)`);
+  rect13.merge(rect13Enter).select('title').text(d => d.key);
+  rect13.exit().remove();
 
-  const rect2 = g.selectAll('.nrw17').data(data.nrw17, (d) => d.key);
-  const rect_enter2 = rect2.enter().append('rect')
-    .attr('x', 0) //set intelligent default values for animation
+  const rect17 = g.selectAll('.nrw17').data(data.nrw17, d => d.key);
+  const rect17Enter = rect17.enter().append('rect')
+    .attr('x', 0)
     .attr('y', 0)
-    .classed("nrw17",true)
+    .classed('nrw17', true)
     .attr('width', xscale.bandwidth())
     .attr('height', 0);
-  rect_enter2.append('title');
-  rect2.merge(rect_enter2).transition()
-    .attr('height', (d) => height - yscale(d.value))
-    .attr('y', (d) => yscale(d.value))
+  rect17Enter.append('title');
+  rect17.merge(rect17Enter).transition()
+    .attr('height', d => height - yscale(d.value))
+    .attr('y', d => yscale(d.value))
     .attr('width', xscale.bandwidth())
-    .attr('x', (d) => xscale(d.key)+5)
-    .style('fill', (d) => 'hsl('+colors[d.key]+',100%,50%)');
-  rect2.merge(rect_enter2).select('title').text((d) => d.key);
-  rect2.exit().remove();
+    .attr('x', d => xscale(d.key) + 5)
+    .style('fill', d => `hsl(${colors[d.key]},100%,50%)`);
+  rect17.merge(rect17Enter).select('title').text(d => d.key);
+  rect17.exit().remove();
 }
 
-export {barchartCreate, barchartUpdate};
+function barchartUpdate(data13, data17, gkz) {
+  const common = Object.keys(data13[0]).filter(x => Object.keys(data17[0]).includes(x));
+  const data13GKZ = data13.filter(x => x.GKZ === gkz)[0];
+  const data17GKZ = data17.filter(x => x.GKZ === gkz)[0];
+  const nrw13 = [];
+  const nrw17 = [];
 
+  for (let i = 6; i < common.length; i += 1) {
+    nrw13.push({
+      key: common[i],
+      value: data13GKZ[common[i]],
+    });
+  }
 
+  for (let i = 6; i < common.length; i += 1) {
+    nrw17.push({
+      key: common[i],
+      value: data17GKZ[common[i]],
+    });
+  }
+  update({ nrw13, nrw17 });
+}
+
+export { barchartCreate, barchartUpdate };
