26c8e25003348661548bf3fa46a867234a99d5b4
Michael Pilz <michael.pilz@fh-salzburg.ac.at>
2018-01-07 11:51:59 +0100

    bubblechart anhand ggkz generieren

--- src/scripts/charts/bubblechart.js
+++ src/scripts/charts/bubblechart.js
@@ -1,7 +1,82 @@
 import * as d3 from 'd3';
 
-const bubbleChart = class {
-
+let g;
+let svg;
+const margin = {
+  top: 10,
+  bottom: 20,
+  left: 80,
+  right: 60,
 };
+const width = 800 - margin.left - margin.right;
+const height = 600 - margin.top - margin.bottom;
 
-export default bubbleChart;
+function bubbleChartCreate() {
+  svg = d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("class", "bubble");
+}
+
+function update(data) {
+  var diameter = 600;
+  var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+  var bubble = d3.pack(data)
+    .size([diameter, diameter])
+    .padding(1.5);
+
+  var nodes = d3.hierarchy(data)
+    .sum(function(d) { return d.Count; });
+
+  svg.selectAll('g').remove();
+
+  var node = svg.selectAll(".node")
+    .data(bubble(nodes).descendants())
+    .enter()
+    .filter(function(d){ return  !d.children })
+    .append("g")
+    .attr("class", "node")
+    .attr("transform", function(d) {
+      return "translate(" + d.x + "," + d.y + ")";
+    });
+
+  node.append("circle")
+    .attr("r", function(d) {return d.r;})
+    .style("fill", function(d,i) { return color(d.data.Cluster); })
+            
+  node.append("text")
+    .attr("dy", ".2em")
+    .style("text-anchor", "middle")
+    .text(function(d) { return d.data.Name.substring(0, d.r / 3); })
+    .attr("font-family", "sans-serif")
+    .attr("font-size", function(d){return d.r/5;})
+    .attr("fill", "white");
+        
+}
+
+function bubbleChartUpdate(data17, gkz, ebene) { 
+    var firstClusterData = [];
+    var secondClusterData = [];
+    console.log(gkz);
+    for (let i = 0; i < data17.length; i += 1) {
+        if(data17[i].GKZ.substring(1, ebene*2) === gkz.substring(1, ebene*2)) {
+            firstClusterData.push({
+                "Name": data17[i].GKZ,
+                "Count": data17[i].Abgegebene,
+                "Cluster": 1
+              });  
+        } else {
+            secondClusterData.push({
+                "Name": data17[i].GKZ,
+                "Count": data17[i].Abgegebene,
+                "Cluster": 2
+              });
+        }
+
+      }
+      
+    update({"children": [{"Name":"First", "children": firstClusterData }, {"Name":"Second", "children": secondClusterData } ]});
+}
+export { bubbleChartCreate, bubbleChartUpdate };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,21 @@
 import * as d3 from 'd3';
 import { barchartCreate, barchartUpdate } from './charts/barchart';
 // import * as treechart from './charts/treechart';
-// import * as bubblechart from './charts/bubblechart';
+import {bubbleChartCreate, bubbleChartUpdate} from './charts/bubblechart';
 
 d3.json(`${window.location.href}assets/nrw_2017.json`, (error17, json17) => {
   const data17 = json17;
   d3.json(`${window.location.href}assets/nrw_2013.json`, (error13, json13) => {
     const data13 = json13;
-    barchartCreate();
-    barchartUpdate(data13, data17, 'G90000');
+    /*barchartCreate();
+    barchartUpdate(data13, data17, 'G90000');*/
+    bubbleChartCreate();
+    var i = 2;
+    var testInterval = setInterval(function(){ 
+      bubbleChartUpdate(data17, 'G'+i+'0000', 1); i++;
+      if(i==10){
+        clearInterval(testInterval);
+      }
+    }, 1000);
   });
 });
