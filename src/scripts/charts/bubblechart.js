b3a9e7e0b99172dfe5b1434e9c98ca0660f952df
Michael Pilz <michael.pilz@fh-salzburg.ac.at>
2018-01-07 15:59:28 +0100

    bugfix bubble Name

--- src/scripts/charts/bubblechart.js
+++ src/scripts/charts/bubblechart.js
@@ -24,7 +24,8 @@ function update(data) {
   var color = d3.scaleOrdinal(d3.schemeCategory20);
   // transition
   var t = d3.transition()
-  .duration(750);
+  .duration(2000);
+
   var bubble = d3.pack(data)
     .size([diameter, diameter])
     .padding(1.5);
@@ -32,51 +33,35 @@ function update(data) {
   var nodes = d3.hierarchy(data)
     .sum(function(d) { return d.Count; });
 
-
   //JOIN
   var circle = svg.selectAll("circle")
-  .data(bubble(nodes).leaves(), function(d){ return d.data.name; });
+    .data(bubble(nodes).leaves(), function(d){ return d.data.Name; });
 
-//EXIT
-circle.exit()
-  .style("fill", function(d,i) { return color(d.data.Cluster); })
-.transition(t)
-  .attr("r", 1e-6)
-  .remove();
-
-//UPDATE
-circle
-.transition(t)
-  .style("fill", function(d,i) { return color(d.data.Cluster); })
-  .attr("r", function(d){ return d.r })
-  .attr("cx", function(d){ return d.x; })
-  .attr("cy", function(d){ return d.y; })
-
-//ENTER
-circle.enter().append("circle")
-  .attr("r", 1e-6)
-  .attr("cx", function(d){ return d.x; })
-  .attr("cy", function(d){ return d.y; })
-  .style("fill", function(d,i) { return color(d.data.Cluster); })
-.transition(t)
-  .style("fill", function(d,i) { return color(d.data.Cluster); })
-  .attr("r", function(d){ return d.r });
-
-    /*
-  svg.selectAll('circle').remove();
-
-  var node = svg.selectAll(".node")
-    .data(bubble(nodes).descendants())
-    .enter()
-    .filter(function(d){ return  !d.children });
-
-  node.append("circle")
-    .attr("r", function(d) {return d.r;})
+  //EXIT
+  circle.exit()
     .style("fill", function(d,i) { return color(d.data.Cluster); })
-    .attr("class", "node")
-    .attr("transform", function(d) {
-      return "translate(" + d.x + "," + d.y + ")";
-    }); */
+    .transition(t)
+    .attr("r", 1e-6)
+    .remove();
+
+  //UPDATE
+  circle
+    .transition(t)
+    .style("fill", function(d,i) { return color(d.data.Cluster); })
+    .attr("r", function(d){ return d.r })
+    .attr("cx", function(d){ return d.x; })
+    .attr("cy", function(d){ return d.y; })
+
+  //ENTER
+  circle.enter().append("circle")
+    .attr("r", 1e-6)
+    .attr("cx", function(d){ return d.x; })
+    .attr("cy", function(d){ return d.y; })
+    .style("fill", function(d,i) { return color(d.data.Cluster); })
+    .transition(t)
+    .style("fill", function(d,i) { return color(d.data.Cluster); })
+    .attr("r", function(d){ return d.r });
+
 }
 
 function bubbleChartUpdate(data17, gkz, ebene) { 
