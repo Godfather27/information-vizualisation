51347cad3dda32a7073c4526f880213e6667892e
Sebastian Huber <sebastian_christoph_huber@yahoo.de>
2018-01-08 13:28:13 +0100

    feat: animate treechart

--- src/scripts/charts/treechart.js
+++ src/scripts/charts/treechart.js
@@ -1,12 +1,12 @@
 import * as d3 from 'd3';
 
 // ************** Generate the tree diagram	 *****************
-let width = window.innerWidth;
-let height = window.innerHeight;
+const width = 800;
+const height = 500;
+const duration = 50;
 let treemap;
 let root;
 let svg;
-let duration = 750;
 
 // Collapse the node and all it's children
 function collapse(d) {
@@ -17,7 +17,7 @@ function collapse(d) {
   }
 }
 
-function wrangleData(source){
+function buildGraph(source){
   let data
   source.forEach((result) => {
     if(result.GKZ === "G00000") {
@@ -39,97 +39,81 @@ function wrangleData(source){
 }
 
 export function treechartCreate(source) {
-  let data = wrangleData(source);
+  let data = buildGraph(source);
   
-  // append the svg object to the body of the page
-  // appends a 'group' element to 'svg'
-  // moves the 'group' element to the top left margin
   svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
-    .append("g")
 
-  // declares a tree layout and assigns the size
   treemap = d3.tree().size([height, width]);
-  // Assigns parent, children, height, depth
   root = d3.hierarchy(data);
-  root.x0 = height / 2;
-  root.y0 = 0;
 
-  // Collapse after the second level
   root.children.forEach(collapse);
 
   treechartUpdate(root);
 }
 
+const setManualSpacing = (d, i) => {
+  d.y = d.depth * width / 4
+  if(d.depth === 1) {
+    const statePosition = height / 2 / d.parent.children.length * (i - 1)
+    const marginTop = height / 4
+    d.x = statePosition + marginTop
+  } else if (d.depth === 0) {
+    d.x = height / 2
+  }
+}
+
 export function treechartUpdate(source) {
-    // Assigns the x and y position for the nodes
     let treeData = treemap(root);
   
-    // Compute the new tree layout.
     let nodes = treeData.descendants();
+
+    nodes.forEach(setManualSpacing);
   
-    // Normalize for fixed-depth.
-    nodes.forEach(d => { d.y = d.depth * 180 });
-  
-    // ****************** Nodes section ***************************
-  
-    // Update the nodes...
     let node = svg.selectAll('g.node')
-        .data(nodes, (d) => d.GKZ);
+        .data(nodes, d => d.data.GKZ);
   
-    // Enter any new nodes at the parent's previous position.
     let nodeEnter = node.enter().append('g')
       .attr('class', 'node')
-      .attr("transform", (d)=> "translate(" + source.y0 + "," + source.x0 + ")")
+      .attr('transform', d => `translate(${d.y},${d.x})`)
       .on('click', toggleChildren);
+      
+    nodeEnter
+      .attr('fill-opacity', 0)
+      .transition(duration)
+      .attr('fill-opacity', 1)
   
-    // Add labels for the nodes
     nodeEnter.append('text')
         .attr("dy", ".35em")
         .attr("text-anchor", 'start')
-        .text((d) => d.data.Gebietsname);
+        .text(d => d.data.Gebietsname);
   
-    // UPDATE
     let nodeUpdate = nodeEnter.merge(node);
   
-    // Transition to the proper position for the node
-    nodeUpdate.transition()
-      .duration(duration)
-      .attr("transform", (d) => "translate(" + d.y + "," + d.x + ")");
+    nodeUpdate
+      .attr('transform', d => `translate(${d.y},${d.x})`)
   
-    // Remove any exiting nodes
-    let nodeExit = node.exit().transition()
-      .duration(duration)
-      .attr("transform", (d)=> "translate(" + source.y + "," + source.x + ")")
+    let nodeExit = node.exit().transition(duration)
+      .style('fill-opacity', 1e-6)
       .remove();
   
     // On exit reduce the opacity of text labels
     nodeExit.select('text')
       .style('fill-opacity', 1e-6);
-
-    // Store the old positions for transition.
-    nodes.forEach((d) => {
-      d.x0 = d.x;
-      d.y0 = d.y;
-    });
-}
-function closeChildren(d) {
-  d._children = d.children;
-  d.children = null;
 }
 
 function openChildren(d) {
   d.children = d._children;
   d._children = null;
+  if(d.parent) {
+    openChildren(d.parent)
+  }
 }
 
 // Toggle children on click.
 function toggleChildren(d) {
-  if (d.children) {
-    closeChildren(d)
-  } else {
-    openChildren(d)
-  }
+  collapse(root)
+  openChildren(d)
   treechartUpdate(d);
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,16 +9,16 @@ d3.json(`${window.location.href}assets/nrw_2017.json`, (error17, json17) => {
     const data13 = json13;
 
     treechartCreate(data13)
-    /*barchartCreate();
-    barchartUpdate(data13, data17, 'G90000');*/
-    // bubbleChartCreate();
-    // var i = 2;
-    // var testInterval = setInterval(function(){ 
-    //   bubbleChartUpdate(data17, 'G'+i+'0000', 1); i++;
-    //   if(i==10){
-    //     clearInterval(testInterval);
-    //   }
-    // }, 1000);
+    barchartCreate();
+    barchartUpdate(data13, data17, 'G90000');
+    bubbleChartCreate();
+    var i = 2;
+    var testInterval = setInterval(function(){ 
+      bubbleChartUpdate(data17, 'G'+i+'0000', 1); i++;
+      if(i==10){
+        clearInterval(testInterval);
+      }
+    }, 1000);
 
   });
 });
