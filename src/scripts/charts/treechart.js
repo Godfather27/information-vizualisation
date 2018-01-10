dade5e1f829c947bb999496a2da30c5ab7ad25f7
Sebastian Huber <fhs37229@fh-salzburg.ac.at>
2018-01-10 22:05:58 +0100

    docs: comment treechart

--- src/scripts/charts/treechart.js
+++ src/scripts/charts/treechart.js
@@ -4,10 +4,14 @@ import { barchartUpdate } from './barchart';
 import { buildGraph, collapse, openChildren } from './../helper/graph';
 import { bubbleChartUpdate } from './bubblechart';
 
+/**
+ * getter function for currently selected gkz
+ */
 const currentGKZ = () => gkz;
 const width = window.innerWidth * 0.55;
 const height = window.innerHeight;
 const duration = 50;
+const padding = 80;
 let gkz = 'G00000';
 let data13;
 let data17;
@@ -15,16 +19,26 @@ let tree;
 let root;
 let svg;
 
+/**
+ * sets fixed spacing for nodes in tree graph
+ * standard spacing would move nodes inappropriatly
+ */
 const setManualSpacing = (d) => {
   d.y = (d.depth * width) / 4;
   if (d.depth > 0) {
-    d.x = (((height - 80) / (Math.max(d.parent.children.length - 1, 1))) *
-      d.parent.children.findIndex(e => e.data.GKZ === d.data.GKZ)) + 40;
+    d.x = (((height - padding) / (Math.max(d.parent.children.length - 1, 1))) *
+      d.parent.children.findIndex(e => e.data.GKZ === d.data.GKZ)) + (padding / 2);
   } else {
     d.x = height / 2;
   }
 };
 
+/**
+ * eventhandler for click events on nodes
+ * sets current GKZ
+ * closes all nodes but the current one and its parents
+ * updates all charts
+ */
 function openNode(d) {
   gkz = d.data.GKZ;
   collapse(root);
@@ -34,6 +48,11 @@ function openNode(d) {
   bubbleChartUpdate(data17, d.data.GKZ);
 }
 
+/**
+ * sets root hierarchy and descendants
+ * and its descendants
+ * and updates nodes and links
+ */
 function treechartUpdate() {
   const treeData = tree(root);
   const nodes = treeData.descendants();
@@ -42,21 +61,24 @@ function treechartUpdate() {
   updateLinks(treeData, nodes);
 }
 
+/**
+ * selects nodes
+ * registers eventhandler
+ * appends text and circles
+ * sets transitions
+ */
 function updateNodes(nodes) {
   nodes.forEach(setManualSpacing);
 
   const node = svg.selectAll('g.node')
     .data(nodes, d => d.data.GKZ);
 
+  // Enter Phase
   const nodeEnter = node.enter()
     .append('g')
     .attr('class', 'node')
     .attr('transform', d => `translate(${d.y},${d.x})`)
     .on('click', openNode);
-
-  nodeEnter.append('path')
-    .attr('class', 'rect')
-    .attr('d', d => d3.path().rect(d.x, d.y, 50, 25));
 
   nodeEnter
     .attr('fill-opacity', 0)
@@ -73,6 +95,7 @@ function updateNodes(nodes) {
     .attr('text-anchor', 'middle')
     .text(d => d.data.Gebietsname);
 
+  // Update Phase
   const nodeUpdate = nodeEnter.merge(node);
 
   nodeUpdate
@@ -83,20 +106,28 @@ function updateNodes(nodes) {
     .attr('r', 10)
     .style('fill', d => d.data.GKZ === gkz ? '#99f' : '#eee');
 
+  // Exit Phase
   const nodeExit = node.exit()
     .transition(duration)
     .style('fill-opacity', 0)
     .remove();
 
   nodeExit.select('text')
-    .style('fill-opacity', 0);
+    .style('fill-opacity', 0)
+    .remove();
 }
 
+/**
+ * selects links (path between nodes)
+ * appends links
+ * sets transitions
+ */
 function updateLinks(treeData, nodes) {
   const links = treeData.links(nodes);
   const link = svg.selectAll('.link')
     .data(links, d => d.target.GKZ);
 
+  // Enter Phase
   const linkEnter = link.enter().append('path')
     .attr('class', 'link')
     .attr('d', d3.linkHorizontal()
@@ -108,17 +139,24 @@ function updateLinks(treeData, nodes) {
     .transition(duration)
     .style('stroke-opacity', 0.2);
 
+  // Update Phase
   const linkUpdate = link.merge(link)
     .style('stroke-opacity', 0)
     .transition(duration)
     .style('stroke-opacity', 0.2);
 
+  // Exit Phase
   const linkExit = link.exit()
     .transition(duration)
     .style('stroke-opacity', 0)
     .remove();
 }
 
+/**
+ * sets global variables
+ * creates and appends svg element to DOM
+ * creates tree and hierarchy
+ */
 function treechartCreate(source13, source17) {
   const data = buildGraph(source17);
   data17 = source17;
@@ -134,6 +172,7 @@ function treechartCreate(source13, source17) {
   tree = d3.tree().size([height, width - 100]);
   root = d3.hierarchy(data);
 
+  // initially close all leafs
   root.children.forEach(collapse);
 
   treechartUpdate();
